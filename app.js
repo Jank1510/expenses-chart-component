0a3855b3b942c8c33a407a4479fce75645fef4bf
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-06-30 20:55:37 -0500

    update

--- app.js
+++
@@ -1,9 +0,0 @@
-var mon = document.getElementById("mon")
-mon.style.height = '100px'
-
-fetch('./data.json')
-    .then(response => response.json())
-    .then(json => {
-        jsonAdvice = json
-        console.log(jsonAdvice)
-    })
